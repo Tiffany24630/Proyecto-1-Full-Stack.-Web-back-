775c1e58bca90af852aadfe579fd61ad70b10cee
Tiffany24630 <sal24630@uvg.edu.gt>
2026-04-21 23:47:28 -0600

    Add OpenAPI documentation and Swagger UI integration for series management

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"net/http"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func main() {
 	initDB()
+
+	http.Handle("/swagger/", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/openapi.yaml"),
+	))
+
+	http.HandleFunc("/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./openapi.yaml")
+	})
 
 	http.HandleFunc("/series", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
